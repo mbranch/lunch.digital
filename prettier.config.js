c9865c575561984b67cdfcd6fbefaaf7f69cb8a8
Michael Branch <michael.branch@toggle.is>
2022-03-03 11:32:25 -0800

    feat: initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: "always",
+	bracketSpacing: true,
+	printWidth: 100,
+	quoteProps: "consistent",
+	semi: false,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "all",
+	useTabs: true,
+};
